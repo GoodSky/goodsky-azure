6efade08e02a020c7373b6a5d60d27c8723a839c
GoodSky <goodsky6@gmail.com>
2016-03-13 13:08:58 -0700

    Okay! I think I fixed the bugs. Apparently the lambda () => syntax and also template string `$(...)` don't work in azure's node.js? I probably need to specify something in the engine to allow those features, but for now I'm trying to remove them.

--- server.js
+++ server.js
@@ -2,75 +2,106 @@
 /// <reference path="./DefinitelyTyped/express/express.d.ts" />
 /// <reference path="./utils.ts" />
 var q = require('q');
-// var fs = require('fs');
-// var http = require('http');
+var fs = require('fs');
+var http = require('http');
 var express = require('express');
-// var azure = require('azure-storage');
-// var utils = require('./utils');
+var azure = require('azure-storage');
+var utils = require('./utils');
 var app = express();
 var home = encodeURI('23216 27th Dr SE Bothell WA 98021');
 var work = encodeURI('City Center Bellevue WA');
 // set up the azure table connection class
 // http://azure.github.io/azure-storage-node/
 // https://azure.microsoft.com/en-us/documentation/articles/storage-nodejs-how-to-use-table-storage/
-// var setupAzureTable = function()
-// {
-//     var tableService;
-//     var file = fs.readFileSync('_private/azure.txt');
-//     if (file)
-//     {
-//         var creds = file.toString().split(',');
-//         var account = creds[0];
-//         var key = creds[1];
-//         tableService = azure.createTableService(account, key);
-//     }
-//     else
-//     {
-//         tableService = azure.createTableService();
-//     }
-//     return tableService;
-// }
-// // query bing maps api for traffic times
-// var queryBing = function(start, end, result)
-// {
-//     var deferred = q.defer();
-//     var bingKey = fs.readFileSync('_private/bing.txt');
-//     var options = {
-//         host: 'dev.virtualearth.net',
-//         path: `/REST/V1/Routes/Driving?wp.0=${start}&wp.1=${end}&key=${bingKey}`
-//     }
-//     var aggregatedData = "";
-//     http.get(options, function(res) {
-//        res.on('data', (chunk) => {
-//            aggregatedData += chunk;
-//        });
-//        res.on('end', () => {
-//            result.data = aggregatedData;
-//            deferred.resolve();
-//        });
-//     }).on('error', function(e) {
-//         console.log('error: ' + e);
-//         deferred.reject(e);
-//     });
-//     return deferred.promise;
-// }
+var setupAzureTable = function () {
+    var tableService;
+    var file = fs.readFileSync('_private/azure.txt');
+    if (file) {
+        var creds = file.toString().split(',');
+        var account = creds[0];
+        var key = creds[1];
+        tableService = azure.createTableService(account, key);
+    }
+    else {
+        tableService = azure.createTableService();
+    }
+    return tableService;
+};
+// query bing maps api for traffic times
+var queryBing = function (start, end) {
+    var deferred = q.defer();
+    var bingKey = fs.readFileSync('_private/bing.txt');
+    var options = {
+        host: 'dev.virtualearth.net',
+        path: '/REST/V1/Routes/Driving?wp.0=' + start + '&wp.1=' + end + '&key=' + bingKey
+    };
+    var aggregatedData = "";
+    http.get(options, function (res) {
+        res.on('data', function (chunk) {
+            aggregatedData += chunk;
+        });
+        res.on('end', function () {
+            deferred.resolve(aggregatedData);
+        });
+    }).on('error', function (e) {
+        console.log('error: ' + e);
+        deferred.reject(e);
+    });
+    return deferred.promise;
+};
+// publish some data to azure table
+// SPECIFICALLY BING MAPS DATA
+var publishBingDataToAzureTable = function (data) {
+    var deferred = q.defer();
+    var obj = JSON.parse(data);
+    var resources = obj.resourceSets[0].resources[0];
+    var distanceKm = resources.travelDistance;
+    var distanceMi = distanceKm * 0.62137119;
+    var estimateSec = resources.travelDuration;
+    var estimateMin = estimateSec / 60.0;
+    var estimateTrafficSec = resources.travelDurationTraffic;
+    var estimateTrafficMin = estimateTrafficSec / 60.0;
+    console.log('distance: ' + distanceKm + ' Km (' + distanceMi + ' Miles)');
+    console.log('time without traffic: ' + estimateSec + ' Secs (' + estimateMin + ' Mins)');
+    console.log('time with traffic: ' + estimateTrafficSec + ' Secs (' + estimateTrafficMin + ' Mins)');
+    var trafficRow = {
+        PartitionKey: { '_': utils.Utils.getCurrentDate() },
+        RowKey: { '_': utils.Utils.getFullDateString() },
+        distance: { '_': '' + distanceMi },
+        time: { '_': '' + estimateMin },
+        trafficTime: { '_': '' + estimateTrafficMin }
+    };
+    // Store in Azure Table
+    var tableService = setupAzureTable();
+    tableService.createTableIfNotExists("traffictimes", function (error) {
+        if (error) {
+            deferred.reject(error);
+        }
+        tableService.insertEntity("traffictimes", trafficRow, function (error, result, response) {
+            if (error) {
+                deferred.reject(error);
+            }
+            deferred.resolve();
+        });
+    });
+    return deferred.promise;
+};
 // // query google maps api for traffic times
-// var queryGoogle = function(start, end, result)
+// var queryGoogle = function(start, end)
 // {
 //     var deferred = q.defer();
 //     var googleKey = fs.readFileSync('_private/google.txt');
 //     var options = {
 //         host: 'maps.googleapis.com',
-//         path: `/maps/api/directions/json?origin=${start}&destination=${end}&key=${googleKey}`
+//         path: '/maps/api/directions/json?origin=' + start + '&destination=' + end + '&key=' + googleKey
 //     }
 //     var aggregatedData = "";
 //     http.get(options, function(res) {
-//        res.on('data', (chunk) => {
+//        res.on('data', function(chunk) {
 //            aggregatedData += chunk;
 //        });
-//        res.on('end', () => {
-//            result.data = aggregatedData;
-//            deferred.resolve();
+//        res.on('end', function() {
+//            deferred.resolve(aggregatedData);
 //        });
 //     }).on('error', function(e) {
 //         console.log('error: ' + e);
@@ -80,14 +111,8 @@ var work = encodeURI('City Center Bellevue WA');
 // }
 // // Query the NOAA public weather API to retrieve weather at the zip code
 // // http://graphical.weather.gov/xml/rest.php#use_it
-// var queryNOAA = function(zip, begin, end, result)
+// var queryNOAA = function(zip, begin, end)
 // {
-//     //    var result = { data: "" };
-//     //    queryNOAA('98021', utils.Utils.getDateString(5), utils.Utils.getDateString(), result)
-//     //         .then(() => {
-//     //                 console.log(`Got Result: ${result.data}`);
-//     //                 res.send(200);
-//     //             });
 //     var deferred = q.defer();
 //     var options = {
 //         host: 'graphical.weather.gov',
@@ -95,12 +120,11 @@ var work = encodeURI('City Center Bellevue WA');
 //     }
 //     var aggregatedData = "";
 //     http.get(options, function(res) {
-//        res.on('data', (chunk) => {
+//        res.on('data', function(chunk) {
 //            aggregatedData += chunk;
 //        });
-//        res.on('end', () => {
-//            result.data = aggregatedData;
-//            deferred.resolve();
+//        res.on('end', function() {
+//            deferred.resolve(aggregatedData);
 //        });
 //     }).on('error', function(e) {
 //         console.log('error: ' + e);
@@ -108,11 +132,6 @@ var work = encodeURI('City Center Bellevue WA');
 //     });
 //     return deferred.promise;
 // }
-var testQ = function () {
-    var deferred = q.defer();
-    setTimeout(function () { deferred.resolve(); }, 2000);
-    return deferred.promise;
-};
 // * * * * * * * * * * * * * * * * * * * * * * * * 
 // URI: /ping
 // ping the service to make sure it is up
@@ -131,10 +150,9 @@ app.get("/ping", function (req, res) {
 // // * * * * * * * * * * * * * * * * * * * * * * * * 
 app.get("/trafficmon", function (req, res) {
     var direction = req.query.direction;
-    var store = req.query.store;
-    if (!direction || !store) {
+    if (!direction) {
         res.status(400);
-        res.send('malformed request! requires direction and store flag');
+        res.send('malformed request! requires direction flag (towork | tohome)');
         return;
     }
     var start;
@@ -150,62 +168,23 @@ app.get("/trafficmon", function (req, res) {
             break;
         default:
             res.status(400);
-            res.send('unrecognized direction');
+            res.send('unrecognized direction. options (towork | tohome)');
             return;
     }
-    testQ()
-        .then(testQ())
-        .then(function () {
-        res.send("Done!");
-    });
-    // this try-catch is a terrible way to debug...
-    //    try {
-    //    var bingResult = { data: "" };
-    //    queryBing(start, end, bingResult)
-    //         .then(() => {
-    //             var obj = JSON.parse(bingResult.data);
-    //             var resources  = obj.resourceSets[0].resources[0];
-    //             var distanceKm = resources.travelDistance;
-    //             var distanceMi = distanceKm * 0.62137119;
-    //             var estimateSec = resources.travelDuration;
-    //             var estimateMin = estimateSec / 60.0;
-    //             var estimateTrafficSec = resources.travelDurationTraffic;
-    //             var estimateTrafficMin = estimateTrafficSec / 60.0;
-    //             console.log(`distance: ${distanceKm} Km (${distanceMi} Miles)`);
-    //             console.log(`time without traffic: ${estimateSec} Secs (${estimateMin} Mins)`);
-    //             console.log(`time with traffic: ${estimateTrafficSec} Secs (${estimateTrafficMin} Mins)`);
-    //             var trafficRow = {
-    //                 PartitionKey: { '_' : utils.Utils.getCurrentDate() },
-    //                 RowKey: { '_' : utils.Utils.getFullDateString() },
-    //                 distance: { '_' : `${distanceMi}` },
-    //                 time: { '_' : `${estimateMin}` },
-    //                 trafficTime: { '_' : `${estimateTrafficMin}` }
-    //             };
-    //             // Store in Azure Table
-    //             var tableService = setupAzureTable();
-    //             tableService.createTableIfNotExists("traffictimes", function(error) { 
-    //                     if (error) {
-    //                         console.log(error); 
-    //                     }
-    //                     tableService.insertEntity("traffictimes", trafficRow, function (error, result, response) {
-    //                         console.log("Azure Table Result: " + result);
-    //                         console.log("Azure Table Response: " + response);
-    //                         if (error) {
-    //                             console.log("Azure Table Error: " + error);
-    //                             res.send(500);
-    //                         }
-    //                         res.send(200);
-    //                     });
-    //             });
-    //         })
-    //         .catch( (error) => {
-    //             console.log(`Q sequence caught error ${error}`);
-    //             res.send(500);
-    //         });
-    //     } catch (err)
-    //     {
-    //         res.send(err);
-    //     }
+    try {
+        queryBing(start, end)
+            .then(publishBingDataToAzureTable)
+            .catch(function (error) {
+            res.status(500);
+            res.send('Q ERROR: ' + error);
+        });
+        res.status(200);
+        res.send('Success! Published data to Azure');
+    }
+    catch (err) {
+        res.status(500);
+        res.send('Main Block ERROR: ' + err);
+    }
 });
 // * * * * * * * * * * * * * * * * * * * * * * * * 
 // Start running Express node.js application
