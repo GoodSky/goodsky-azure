821d1696bfdeb0837395ad4e8913effc9a9e2bd5
GoodSky <goodsky6@gmail.com>
2016-03-12 17:39:15 -0800

    K-C style debugging

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var http = require('http');
 var express = require('express');
 var azure = require('azure-storage');
-var settings = require('mobileservice-config').appSettings;
+// var settings = require('mobileservice-config').appSettings;
 var utils = require('./utils');
 var app = express();
 var home = encodeURI('23216 27th Dr SE Bothell WA 98021');
@@ -32,12 +32,14 @@ var setupAzureTable = function () {
 var queryBing = function (start, end, result) {
     var deferred = q.defer();
     var bingKey = fs.readFileSync('_private/bing.txt');
-    if (!bingKey) {
-        bingKey = settings.BING_KEY;
-        if (!bingKey) {
-            console.log("COULD NOT READ BING KEY");
-        }
-    }
+    // if (!bingKey)
+    // {
+    //     bingKey = settings.BING_KEY;
+    //     if (!bingKey)
+    //     {
+    //         console.log("COULD NOT READ BING KEY");
+    //     }
+    // }
     var options = {
         host: 'dev.virtualearth.net',
         path: `/REST/V1/Routes/Driving?wp.0=${start}&wp.1=${end}&key=${bingKey}`
@@ -61,12 +63,14 @@ var queryBing = function (start, end, result) {
 var queryGoogle = function (start, end, result) {
     var deferred = q.defer();
     var googleKey = fs.readFileSync('_private/google.txt');
-    if (!googleKey) {
-        googleKey = settings.GOOGLE_KEY;
-        if (!googleKey) {
-            console.log("COULD NOT READ GOOGLE KEY");
-        }
-    }
+    // if (!googleKey)
+    // {
+    //     googleKey = settings.GOOGLE_KEY;
+    //     if (!googleKey)
+    //     {
+    //         console.log("COULD NOT READ GOOGLE KEY");
+    //     }
+    // }
     var options = {
         host: 'maps.googleapis.com',
         path: `/maps/api/directions/json?origin=${start}&destination=${end}&key=${googleKey}`
@@ -155,48 +159,54 @@ app.get("/trafficmon", function (req, res) {
             res.send('unrecognized direction');
             return;
     }
-    var bingResult = { data: "" };
-    queryBing(start, end, bingResult)
-        .then(() => {
-        var obj = JSON.parse(bingResult.data);
-        var resources = obj.resourceSets[0].resources[0];
-        var distanceKm = resources.travelDistance;
-        var distanceMi = distanceKm * 0.62137119;
-        var estimateSec = resources.travelDuration;
-        var estimateMin = estimateSec / 60.0;
-        var estimateTrafficSec = resources.travelDurationTraffic;
-        var estimateTrafficMin = estimateTrafficSec / 60.0;
-        console.log(`distance: ${distanceKm} Km (${distanceMi} Miles)`);
-        console.log(`time without traffic: ${estimateSec} Secs (${estimateMin} Mins)`);
-        console.log(`time with traffic: ${estimateTrafficSec} Secs (${estimateTrafficMin} Mins)`);
-        var trafficRow = {
-            PartitionKey: { '_': utils.Utils.getCurrentDate() },
-            RowKey: { '_': utils.Utils.getFullDateString() },
-            distance: { '_': `${distanceMi}` },
-            time: { '_': `${estimateMin}` },
-            trafficTime: { '_': `${estimateTrafficMin}` }
-        };
-        // Store in Azure Table
-        var tableService = setupAzureTable();
-        tableService.createTableIfNotExists("traffictimes", function (error) {
-            if (error) {
-                console.log(error);
-            }
-            tableService.insertEntity("traffictimes", trafficRow, function (error, result, response) {
-                console.log("Azure Table Result: " + result);
-                console.log("Azure Table Response: " + response);
+    // this try-catch is a terrible way to debug...
+    try {
+        var bingResult = { data: "" };
+        queryBing(start, end, bingResult)
+            .then(() => {
+            var obj = JSON.parse(bingResult.data);
+            var resources = obj.resourceSets[0].resources[0];
+            var distanceKm = resources.travelDistance;
+            var distanceMi = distanceKm * 0.62137119;
+            var estimateSec = resources.travelDuration;
+            var estimateMin = estimateSec / 60.0;
+            var estimateTrafficSec = resources.travelDurationTraffic;
+            var estimateTrafficMin = estimateTrafficSec / 60.0;
+            console.log(`distance: ${distanceKm} Km (${distanceMi} Miles)`);
+            console.log(`time without traffic: ${estimateSec} Secs (${estimateMin} Mins)`);
+            console.log(`time with traffic: ${estimateTrafficSec} Secs (${estimateTrafficMin} Mins)`);
+            var trafficRow = {
+                PartitionKey: { '_': utils.Utils.getCurrentDate() },
+                RowKey: { '_': utils.Utils.getFullDateString() },
+                distance: { '_': `${distanceMi}` },
+                time: { '_': `${estimateMin}` },
+                trafficTime: { '_': `${estimateTrafficMin}` }
+            };
+            // Store in Azure Table
+            var tableService = setupAzureTable();
+            tableService.createTableIfNotExists("traffictimes", function (error) {
                 if (error) {
-                    console.log("Azure Table Error: " + error);
-                    res.send(500);
+                    console.log(error);
                 }
-                res.send(200);
+                tableService.insertEntity("traffictimes", trafficRow, function (error, result, response) {
+                    console.log("Azure Table Result: " + result);
+                    console.log("Azure Table Response: " + response);
+                    if (error) {
+                        console.log("Azure Table Error: " + error);
+                        res.send(500);
+                    }
+                    res.send(200);
+                });
             });
+        })
+            .catch((error) => {
+            console.log(`Q sequence caught error ${error}`);
+            res.send(500);
         });
-    })
-        .catch((error) => {
-        console.log(`Q sequence caught error ${error}`);
-        res.send(500);
-    });
+    }
+    catch (err) {
+        res.send(err);
+    }
 });
 // * * * * * * * * * * * * * * * * * * * * * * * * 
 // Start running Express node.js application
